c3c1e55c202544ce4a1387c481a1536e03c4973a
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 13:36:28 +0300

    created more content and styles

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { StyledLogo, StyledLogoImgSection } from './RegistrationForm.style';
+import Button from './Button';
+import Form from './Form';
+import {
+  StyledLogo,
+  StyledLogoImgSection,
+  StyledTextDiv,
+  StyledMain,
+  StyledFormSection,
+  StyledA,
+  StyledTextButtonsDiv,
+  StyledButtonDiv,
+  StyledH3,
+  StyledGoogleIcon,
+  StyledFacebookIcon,
+  StyledImgDiv,
+  StyledImg,
+} from './RegistrationForm.style';
+import { FaGoogle, FaFacebook } from 'react-icons/fa';
 
 const RegistrationForm = () => {
   return (
-    <main>
+    <StyledMain>
       <StyledLogoImgSection>
         <StyledLogo>
           <div>
@@ -13,14 +30,46 @@ const RegistrationForm = () => {
             />
           </div>
         </StyledLogo>
-        <div>
-          <img
+        <StyledImgDiv>
+          <StyledImg
             src='https://raw.githubusercontent.com/JoyShaheb/Project-image-repo/e2aa9d5d409b28efe7040e4b92da891c64ee23ce/Form-Validation/images/illustration.svg'
             alt=''
           />
-        </div>
+        </StyledImgDiv>
+        <StyledTextDiv>
+          <p>Start for free & Get Attractive offers Today !</p>
+        </StyledTextDiv>
       </StyledLogoImgSection>
-    </main>
+      <StyledFormSection>
+        <StyledTextButtonsDiv>
+          <h2>Get Started</h2>
+          <p>Already have an account?</p>
+          <StyledA href='close'>Log in</StyledA>
+          <StyledButtonDiv>
+            <Button
+              icon={
+                <StyledGoogleIcon>
+                  <FaGoogle />
+                </StyledGoogleIcon>
+              }
+              text='sign up'
+              primary
+            />
+            <Button
+              icon={
+                <StyledFacebookIcon>
+                  <FaFacebook />
+                </StyledFacebookIcon>
+              }
+              text='sign up'
+              secondary
+            />
+          </StyledButtonDiv>
+          <StyledH3>Or</StyledH3>
+        </StyledTextButtonsDiv>
+        <Form />
+      </StyledFormSection>
+    </StyledMain>
   );
 };
 
