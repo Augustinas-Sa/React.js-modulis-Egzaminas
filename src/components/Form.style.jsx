f287dcb8c2c32dcae8f221abb21d9ee85b81efb7
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 13:35:56 +0300

    created form style

---
+++ src/components/Form.style.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const FormInput = styled.input`
+  padding-left: 1.9rem;
+  border-radius: 5px;
+  border: none;
+  width: 100%;
+  height: 30px;
+  border: 1px solid #afafaf;
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  height: 400px;
+  width: 400px;
+  padding: 10px;
+  margin-top: 50px;
+`;
+
+export const StyledSubmitInput = styled.input`
+  background-color: #f2877d;
+  color: white;
+  border: none;
+  cursor: pointer;
+  height: 30px;
+  border-radius: 5px;
+  margin-right: 10px;
+  width: 100%;
+`;
+
+export const StyledSubmitDiv = styled.div`
+  margin-top: 20px;
+`;
+
+export const StyledFormDiv = styled.div`
+  margin-bottom: 5px;
+`;
