bf23841152cd3cec4228a5a5258c100dc7449935
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 13:36:45 +0300

    created more styles

--- src/components/RegistrationForm.style.jsx
+++ src/components/RegistrationForm.style.jsx
@@ -10,3 +10,70 @@ export const StyledLogoImgSection = styled.section`
   border-radius: 10px;
   width: 500px;
 `;
+
+export const StyledTextDiv = styled.div`
+  color: white;
+  width: 210px;
+  text-align: center;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
+export const StyledMain = styled.main`
+  display: flex;
+  border: 1px solid #f2877d;
+  border-radius: 10px;
+  height: 600px;
+  width: 1000px;
+  margin: 0 auto;
+`;
+
+export const StyledFormSection = styled.section`
+  width: 500px;
+  padding: 50px;
+`;
+
+export const StyledA = styled.a`
+  text-decoration: none;
+  color: #f2877d;
+`;
+
+export const StyledTextButtonsDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 400px;
+  padding: 10px;
+`;
+
+export const StyledButtonDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+export const StyledH3 = styled.h3`
+  text-align: center;
+`;
+
+export const StyledGoogleIcon = styled.div`
+  margin-right: 10px;
+`;
+
+export const StyledFacebookIcon = styled.div`
+  margin-right: 10px;
+  color: white;
+`;
+
+export const StyledImgDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const StyledImg = styled.img`
+  height: 400px;
+  width: 400px;
+`;
