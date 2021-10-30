99e2e13be831d3ca2c38dfae48fb5549ce0fc51e
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 11:13:44 +0300

    added styles to complete section

---
+++ src/components/RegistrationForm.style.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledLogo = styled.div`
+  display: flex;
+  padding: 30px;
+`;
+
+export const StyledLogoImgSection = styled.section`
+  background-color: #f2877d;
+  border-radius: 10px;
+  width: 500px;
+`;
