10cd64274a8ed711a320ed61661911146d146e58
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 11:12:03 +0300

    rendering RegistrationForm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import RegistrationForm from './components/RegistrationForm';
 
 function App() {
   return (
     <>
-      <div className='App'></div>
+      <div className='App'>
+        <RegistrationForm />
+      </div>
     </>
   );
 }
