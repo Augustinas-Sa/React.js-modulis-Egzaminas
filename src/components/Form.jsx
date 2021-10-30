d01a440d4f55bb53bef314e812f2c1d29ed7dc07
Augustinas-Sa <82833472+Augustinas-Sa@users.noreply.github.com>
2021-10-30 13:35:42 +0300

    created form content

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import {
+  FormInput,
+  StyledForm,
+  StyledSubmitInput,
+  StyledSubmitDiv,
+  StyledFormDiv,
+} from './Form.style';
+import { FaEnvelope, FaUser, FaLock } from 'react-icons/fa';
 
 const Form = () => {
-  return <form></form>;
+  return (
+    <StyledForm>
+      <StyledFormDiv>
+        <label htmlFor='name'>Name</label>
+        <br />
+        <FaUser
+          style={{
+            marginLeft: '0.5rem',
+            position: 'absolute',
+            marginTop: '0.4rem',
+          }}
+          color='#afafaf'
+          size='1em'
+        />
+        <FormInput type='text' placeholder='Joy Shaheb' />
+      </StyledFormDiv>
+
+      <StyledFormDiv>
+        <label htmlFor='email'>Email</label>
+        <br />
+        <FaEnvelope
+          style={{
+            marginLeft: '0.5rem',
+            position: 'absolute',
+            marginTop: '0.5rem',
+          }}
+          color='#afafaf'
+          size='1em'
+        />
+        <FormInput type='email' placeholder='abc@gmail.com' />
+      </StyledFormDiv>
+
+      <StyledFormDiv>
+        <label htmlFor='password'>Password</label>
+        <br />
+        <FaLock
+          style={{
+            marginLeft: '0.5rem',
+            position: 'absolute',
+            marginTop: '0.4rem',
+          }}
+          color='#afafaf'
+          size='1em'
+        />
+        <FormInput type='password' placeholder='.......' />
+      </StyledFormDiv>
+
+      <StyledSubmitDiv>
+        <StyledSubmitInput type='submit' value='Submit' />
+      </StyledSubmitDiv>
+    </StyledForm>
+  );
 };
 
 export default Form;
